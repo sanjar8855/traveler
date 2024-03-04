98a1f55a2a8d291947291ac5942dc42707c30fa5
sanjar <38586745+sanjar8855@users.noreply.github.com>
2024-03-04 16:27:16 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// let serials = ['aaa', 'bbb', 'ccc', 'a','b','c','ii']
+
+
+let arr = {
+    ism:'aaa',
+    bb:'bbb'
+}
+let objToJson = JSON.stringify(arr)
+console.log(arr)
+console.log(objToJson)
